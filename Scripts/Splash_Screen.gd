3b79a45b75d022022f152d38be2c4c5d03181fb2
xArtix32 <58748700+xArtix32@users.noreply.github.com>
2020-02-12 21:33:54 +0100

    Script Changes
    The Script for the Splash Screen has changed, Here a changelog:
    -Added a new bool type variable, can_skip.
    -Created 2 specific function for fade in/fade out the screen.
    -Enabled the process_input.
    -Set a condition in the _input(event) function, for skip the delay between the fade in/fade out animation, by pressing the "Enter" key.

--- Scripts/Splash_Screen.gd
+++ Scripts/Splash_Screen.gd
@@ -3,22 +3,45 @@ extends Control
 onready var sc_sprite : Sprite = $image
 onready var sc_animations : AnimationPlayer = $animations
 onready var sc_timer : Timer = $time
+var can_skip : bool = false
 var ts_scene = "res://Scenes/Title_screen.tscn"
 
 func _ready():
 	sc_sprite.modulate = Color(0,0,0,1)
-	sc_animations.play("fade_in")
+	fade_in_screen()
+	set_process_input(true)
 	pass 
 
 
+func _input(event):
+	if event.is_action_pressed("ui_accept") and can_skip:
+		if sc_timer.time_left < sc_timer.wait_time:
+			sc_timer.stop()
+			fade_out_screen()
+			can_skip = false
+	pass
+
+
 func _on_time_timeout():
+	fade_out_screen()
+	can_skip = false
+	pass
+
+
+func fade_out_screen():
 	sc_animations.play("fade_out")
+	pass
+
+
+func fade_in_screen():
+	sc_animations.play("fade_in")
 	pass
 
 
 func _on_animations_animation_finished(anim_name):
 	if anim_name == "fade_in":
 		sc_timer.start()
+		can_skip = true
 	if anim_name == "fade_out":
 		get_tree().change_scene(ts_scene)
 	pass
