26929aaa295816ca406c60cea80a3e6fe2c180e3
xArtix32 <58748700+xArtix32@users.noreply.github.com>
2020-02-16 14:13:00 +0100

    Changed in the title screen script
    Here a changelog:
    -Added a new condition at line 47:
    "With this condition, now you can't hover the buttons if the fade in animations is not over."
    -Now the c_hover node has a new animation by tween.

--- Scripts/Title_screen.gd
+++ Scripts/Title_screen.gd
@@ -7,6 +7,7 @@ onready var ts_canvas_com : CanvasLayer = $Commands
 onready var c_hover_node : Sprite = $Commands/c_hover
 onready var ts_bgm : AudioStreamPlayer = $bgm
 onready var ts_sfx : AudioStreamPlayer = $sfx
+onready var c_tween : Tween = $Commands/c_hover/tween
 var c_move_sfx : Resource = preload("res://SFX/cursor_move.ogg")
 var ts_is_active : bool = false
 var buttons : Array = []
@@ -32,22 +33,21 @@ func _on_animations_animation_finished(anim_name):
 
 
 func create_cursor():
-	ts_canvas_com.add_child(cursor)
 	c_hover_node.visible = true
+	c_tween.interpolate_property(c_hover_node, "modulate", Color(0,0,0,0),
+	Color(1,1,1,1),0.2,Tween.TRANS_CUBIC,Tween.EASE_IN)
+	c_tween.start()
+	ts_canvas_com.add_child(cursor)
 	set_cursor(0)
 	pass
 
 
 func set_cursor(new_index : int):
-<<<<<<< HEAD
-	if new_index >= 0 and new_index < len(labels):
-=======
 	if new_index >= 0 and new_index < len(buttons):
 		if new_index != index:
 			ts_sfx.stream = c_move_sfx
 			ts_sfx.stream.set("loop",false)
 			ts_sfx.play()
->>>>>>> c4df3b4... Changes in The title screen script
 		index = new_index
 		var selected_command = buttons[index]
 		c_hover_node.global_position = Vector2(
@@ -58,6 +58,22 @@ func set_cursor(new_index : int):
 			selected_command.rect_global_position.x - 25,
 			selected_command.rect_global_position.y + 25
 		)
+	if ts_is_active:
+		if new_index >= 0 and new_index < len(buttons):
+			if new_index != index:
+				ts_sfx.stream = c_move_sfx
+				ts_sfx.stream.set("loop",false)
+				ts_sfx.play()
+			index = new_index
+			var selected_command = buttons[index]
+			c_hover_node.global_position = Vector2(
+				selected_command.rect_global_position.x + 160,
+				selected_command.rect_global_position.y + 28
+			)
+			cursor.rect_global_position = Vector2(
+				selected_command.rect_global_position.x - 25,
+				selected_command.rect_global_position.y + 25
+			)
 	pass
 
 func _process(delta):
