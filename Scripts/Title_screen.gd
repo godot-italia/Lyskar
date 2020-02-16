9223565ee32d2f742ae82415f02b1ab410696990
xArtix32 <58748700+xArtix32@users.noreply.github.com>
2020-02-16 11:25:05 +0100

    Changes in The title screen script
    Here a Changelog:
    -Added a new variable called "c_hover_node".
    -Changed the condition for adding the buttons in the array.
    -Changed some name of variable for better understanding of the script.
    -Set the hover node visibility to true in the create_cursor function.
    -Now the sprite hover follow the cursor.

--- Scripts/Title_screen.gd
+++ Scripts/Title_screen.gd
@@ -4,8 +4,12 @@ onready var ts_color_rect : ColorRect = $CanvasLayer/ColorRect
 onready var ts_animations : AnimationPlayer = $CanvasLayer/animations
 onready var ts_commands : VBoxContainer = $Commands/VBoxContainer
 onready var ts_canvas_com : CanvasLayer = $Commands
+onready var c_hover_node : Sprite = $Commands/c_hover
+onready var ts_bgm : AudioStreamPlayer = $bgm
+onready var ts_sfx : AudioStreamPlayer = $sfx
+var c_move_sfx : Resource = preload("res://SFX/cursor_move.ogg")
 var ts_is_active : bool = false
-var labels : Array = []
+var buttons : Array = []
 var cursor_scene = preload("res://Scenes/Cursor.tscn")
 var cursor = cursor_scene.instance()
 var index = 0
@@ -14,8 +18,8 @@ func _ready():
 	ts_color_rect.color = Color(0,0,0,1)
 	ts_animations.play("fade_in")
 	for node in ts_commands.get_children():
-		if node is Label:
-			labels.append(node)
+		if node is Button:
+			buttons.append(node)
 	pass 
 
 
@@ -28,14 +32,26 @@ func _on_animations_animation_finished(anim_name):
 
 func create_cursor():
 	ts_canvas_com.add_child(cursor)
+	c_hover_node.visible = true
 	set_cursor(0)
 	pass
 
 
 func set_cursor(new_index : int):
+<<<<<<< HEAD
 	if new_index >= 0 and new_index < len(labels):
+=======
+	if new_index >= 0 and new_index < len(buttons):
+		if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down"):
+			ts_sfx.stream = c_move_sfx
+			ts_sfx.play()
+>>>>>>> c4df3b4... Changes in The title screen script
 		index = new_index
-		var selected_command = labels[index]
+		var selected_command = buttons[index]
+		c_hover_node.global_position = Vector2(
+			selected_command.rect_global_position.x + 160,
+			selected_command.rect_global_position.y + 28
+		)
 		cursor.rect_global_position = Vector2(
 			selected_command.rect_global_position.x - 25,
 			selected_command.rect_global_position.y + 25
